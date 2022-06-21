fd383b7627c948654663d2c1f975c621b50a0822
marianabonan <marianabonanbs@gmail.com>
2022-06-20 21:00:27 -0300

    feat: styled global criado

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import GlobalStyled from "./styles/global";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyled />
+      <h1>ooi</h1>
+      <p>
+        {" "}
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi, oi,
+        oi{" "}
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    font-family: 'Montserrat', sans-serif;
+}
+
+:root {
+    --white:#FFFFFF;
+    --grey-0: #0000004D;
+    --grey-1: #707070;
+    --grey-2: #EDEDED;
+    --grey-3: #FAFAFA;
+    --grey-4: #FCFCFC;
+    --purple-0: #1C0055;
+    --purple-1: #4A00E0;
+    --purple-2: #8E2DE2;
+    --purple-3: #9974E5;
+    --purple-4: #B783E5;
+}
+
+p{
+    font-size: 24px;
+    color: var(--grey-1);
+}
+
+h1{
+    font-size: 90px;
+    color: var(--purple-1);
+}
+
+h2{
+    font-size: 60px ;
+}
+
+h3{
+    font-size: 40px ;
+}
+`;
